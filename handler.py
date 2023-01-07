4bd1b3308a4cab623c2048050dcbfc09862d1ecd
Sergryap <rs1180@mail.ru>
2023-01-07 19:25:02 +0500

    start_handler and handle_users_reply functions have been merged

--- handler.py
+++ handler.py
@@ -5,23 +5,8 @@ from messages import send_message, send_hello
 from verify import get_verify_func, verify_hello
 
 
-async def start_handler(event: SimpleBotEvent):
-	user_id = event.user_id
-	os.environ[f'{user_id}_NEXT_STATE'] = 'START_MENU'
-	msg = event.text.lower().strip()
-	api = event.api_ctx
-	user_data = (await api.users.get(user_ids=user_id)).response[0]
-	user_info = {'first_name': user_data.first_name}
-	if verify_hello(msg):
-		await send_hello(event, user_info)
-	for verify, func in get_verify_func().items():
-		if verify(msg):
-			await func(event, user_info)
-
-
 async def handle_main_menu(event: SimpleBotEvent):
 	user_id = event.user_id
-	os.environ[f'{user_id}_NEXT_STATE'] = 'START_MENU'
 	msg = event.text.lower().strip()
 	api = event.api_ctx
 	user_data = (await api.users.get(user_ids=user_id, fields='country,city,bdate,sex')).response[0]
@@ -34,9 +19,11 @@ async def handle_main_menu(event: SimpleBotEvent):
 	}
 	if verify_hello(msg):
 		await send_hello(event, user_info)
-	for verify, func in get_verify_func().items():
-		if verify(msg):
-			await func(event, user_info)
+	for verify_msg, send_msg_func in get_verify_func().items():
+		if verify_msg(msg):
+			await send_msg_func(event, user_info)
+
+	return 'START'
 
 
 async def handle_users_reply(event: SimpleBotEvent):
@@ -46,8 +33,7 @@ async def handle_users_reply(event: SimpleBotEvent):
 	else:
 		user_state = os.environ.get(f'{user_id}_NEXT_STATE', 'START')
 	states_functions = {
-		'START': start_handler,
-		'START_MENU': handle_main_menu
+		'START': handle_main_menu,
 	}
 	state_handler = states_functions[user_state]
-	await state_handler(event)
+	os.environ[f'{user_id}_NEXT_STATE'] = await state_handler(event)
