420c570e6433affe9c8a155a3d4f73eec306e2f7
Sergryap <rs1180@mail.ru>
2023-01-07 21:31:53 +0500

    Sending message to admins has been added

--- handler.py
+++ handler.py
@@ -1,8 +1,13 @@
 import os
+import random
+import time
 from vkwave.bots import SimpleBotEvent
 from textwrap import dedent
 from messages import send_message, send_hello
 from verify import get_verify_func, verify_hello
+
+USER_MSG = []
+MAX_SEND_ADMIN_MSG = 5
 
 
 async def handle_main_menu(event: SimpleBotEvent):
@@ -27,7 +32,19 @@ async def handle_main_menu(event: SimpleBotEvent):
 
 
 async def handle_users_reply(event: SimpleBotEvent):
+	global USER_MSG
 	user_id = event.user_id
+	text = f'от https://vk.com/id{user_id}: "{event.text}"'
+	USER_MSG.append(text)
+	if len(USER_MSG) == MAX_SEND_ADMIN_MSG:
+		api = event.api_ctx
+		msg_head = f'Сообщения в чате https://vk.com/gim142029999:'
+		await api.messages.send(
+			random_id=random.randint(0, 1000),
+			user_ids=os.environ['ADMIN_IDS'].split(),
+			message='\n'.join([msg_head] + USER_MSG)
+		)
+		USER_MSG = []
 	if event.text.lower().strip() in ['start', '/start', 'начать', 'старт']:
 		user_state = 'START'
 	else:
