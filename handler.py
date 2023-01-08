f0cdfc1545e3a7e25aa88c2a96cc0fbda9878316
Sergryap <rs1180@mail.ru>
2023-01-08 11:34:10 +0500

    The Storage class has been added

--- handler.py
+++ handler.py
@@ -1,29 +1,40 @@
 import os
 import random
 from vkwave.bots import SimpleBotEvent
+from vkwave.bots.storage.storages import Storage
+from vkwave.bots.storage.types import Key
 from textwrap import dedent
 from messages import send_hello, send_discount, send_message, send_menu
-from verify import get_verify_func, verify_hello, verify_discount, verify_menu, verify_fsm_break, verify_phone
-from verify import verify_fsm_start
+from verify import (
+	get_verify_func,
+	verify_hello,
+	verify_discount,
+	verify_menu,
+	verify_fsm_break,
+	verify_phone,
+	verify_fsm_start
+)
 from buttons import BTN_DISCOUNT_STEP_4
+
+storage = Storage()
 
 USER_MSG = []
 MAX_SEND_ADMIN_MSG = 5
 CANCELLATION_MESSAGE = 'Вы можете продолжить в любое время. Просто отправьте "получить скидку"'
 
 
-def get_initial_data(event: SimpleBotEvent):
+async def get_initial_data(event: SimpleBotEvent):
 	user_id = event.user_id
 	msg = event.text.lower().strip()
 	user_info = {
-		'first_name': os.environ[f'{user_id}_FIRST_NAME'],
-		'last_name': os.environ[f'{user_id}_LAST_NAME']
+		'first_name': await storage.get(Key(f'{user_id}_first_name')),
+		'last_name': await storage.get(Key(f'{user_id}_last_name'))
 	}
 	return user_id, msg, user_info
 
 
 async def handle_main_menu(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 
 	if verify_hello(msg):
 		await send_hello(event, user_info)
@@ -40,10 +51,11 @@ async def handle_main_menu(event: SimpleBotEvent):
 
 
 async def handle_discount_step_1(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_start(msg):
 		text = f'1. {user_info["first_name"]}, оставьте пожалуйста ваш контактный номер телефона:'
-		os.environ[f'{user_id}_DISCOUNT'] = f'vk: https://vk.com/id{user_id}\n'
+		discount_msg = f'vk: https://vk.com/id{user_id}\n'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		await send_message(event, msg=text, buttons='fsm_quiz')
 		return 'DISCOUNT_2'
 	else:
@@ -58,7 +70,7 @@ async def handle_discount_step_1(event: SimpleBotEvent):
 
 
 async def handle_discount_step_2(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_break(msg):
 		text = f'{user_info["first_name"]} {CANCELLATION_MESSAGE}'
 		await send_message(event, msg=text)
@@ -69,28 +81,30 @@ async def handle_discount_step_2(event: SimpleBotEvent):
 		await send_message(event, msg=text, buttons='fsm_quiz')
 		return 'DISCOUNT_2'
 	else:
-		os.environ[f'{user_id}_DISCOUNT'] += f'phone: {msg}\n'
+		discount_msg = await storage.get(Key(f'{user_id}_discount')) + f'phone: {msg}\n'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		text = '2. Введите ваше имя'
 		await send_message(event, msg=text, buttons='fsm_quiz')
 		return 'DISCOUNT_3'
 
 
 async def handle_discount_step_3(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_break(msg):
 		text = f'{user_info["first_name"]} {CANCELLATION_MESSAGE}'
 		await send_message(event, msg=text)
 		await send_menu(event, user_info)
 		return 'START'
 	else:
-		os.environ[f'{user_id}_DISCOUNT'] += f'name: {msg}\n'
+		discount_msg = await storage.get(Key(f'{user_id}_discount')) + f'name: {msg}\n'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		text = '3. Как вы нас нашли? Выберите ниже, либо напишите свой вариант'
 		await send_message(event, msg=text, buttons='search')
 		return 'DISCOUNT_4'
 
 
 async def handle_discount_step_4(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_break(msg):
 		text = f'{user_info["first_name"]} {CANCELLATION_MESSAGE}'
 		await send_message(event, msg=text)
@@ -101,35 +115,38 @@ async def handle_discount_step_4(event: SimpleBotEvent):
 		await send_message(event, msg=text, buttons='search')
 		return 'DISCOUNT_4'
 	else:
-		os.environ[f'{user_id}_DISCOUNT'] += f'search: {msg}\n'
+		discount_msg = await storage.get(Key(f'{user_id}_discount')) + f'search: {msg}\n'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		text = '4. Кем вы сейчас работаете или чем занимаетесь? Выберите ниже или напишите свой вариант.'
 		await send_message(event, msg=text, buttons='what_job')
 		return 'DISCOUNT_5'
 
 
 async def handle_discount_step_5(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_break(msg):
 		text = f'{user_info["first_name"]} {CANCELLATION_MESSAGE}'
 		await send_message(event, msg=text)
 		await send_menu(event, user_info)
 		return 'START'
 	else:
-		os.environ[f'{user_id}_DISCOUNT'] += f'work: {msg}\n'
+		discount_msg = await storage.get(Key(f'{user_id}_discount')) + f'work: {msg}\n'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		text = '5. Укажите ваш возраст, либо пропустите данный пункт.'
 		await send_message(event, msg=text, buttons='fsm_quiz')
 		return 'DISCOUNT_6'
 
 
 async def handle_discount_step_6(event: SimpleBotEvent):
-	user_id, msg, user_info = get_initial_data(event)
+	user_id, msg, user_info = await get_initial_data(event)
 	if verify_fsm_break(msg):
 		text = f'{user_info["first_name"]} {CANCELLATION_MESSAGE}'
 		await send_message(event, msg=text)
 		await send_menu(event, user_info)
 		return 'START'
 	elif msg == "пропустить" or (msg.isdigit() and 10 < int(msg) < 100):
-		os.environ[f'{user_id}_DISCOUNT'] += f'age: {msg}'
+		discount_msg = await storage.get(Key(f'{user_id}_discount')) + f'age: {msg}'
+		await storage.put(Key(f'{user_id}_discount'), discount_msg)
 		api = event.api_ctx
 		text = f'''
 				Спасибо, {user_info["first_name"]}, мы обязательно свяжемся с вами.
@@ -140,9 +157,9 @@ async def handle_discount_step_6(event: SimpleBotEvent):
 		await api.messages.send(
 			random_id=random.randint(0, 1000),
 			user_ids=os.environ['ADMIN_IDS'].split(),
-			message=msg_head + os.environ[f'{user_id}_DISCOUNT']
+			message=msg_head + discount_msg
 		)
-		del os.environ[f'{user_id}_DISCOUNT']
+		await storage.delete(Key(f'{user_id}_discount'))
 		return 'START'
 	else:
 		text = f'''
@@ -157,15 +174,13 @@ async def handle_users_reply(event: SimpleBotEvent):
 	global USER_MSG
 	user_id = event.user_id
 	api = event.api_ctx
-
-	if not os.environ.get(f'{user_id}_FIRST_NAME'):
+	if not await storage.contains(Key(f'{user_id}_first_name')):
 		user_data = (await api.users.get(user_ids=user_id)).response[0]
-		os.environ[f'{user_id}_FIRST_NAME'] = user_data.first_name
-		os.environ[f'{user_id}_LAST_NAME'] = user_data.last_name
+		await storage.put(Key(f'{user_id}_first_name'), user_data.first_name)
+		await storage.put(Key(f'{user_id}_last_name'), user_data.last_name)
 
 	text = f'от https://vk.com/id{user_id}: "{event.text}"'
 	USER_MSG.append(text)
-
 	if len(USER_MSG) == MAX_SEND_ADMIN_MSG:
 		msg_head = f'Сообщения в чате https://vk.com/gim142029999:'
 		await api.messages.send(
@@ -178,7 +193,7 @@ async def handle_users_reply(event: SimpleBotEvent):
 	if event.text.lower().strip() in ['start', '/start', 'начать', 'старт']:
 		user_state = 'START'
 	else:
-		user_state = os.environ.get(f'{user_id}_NEXT_STATE', 'START')
+		user_state = await storage.get(Key(f'{user_id}_next_state'), default='START')
 	states_functions = {
 		'START': handle_main_menu,
 		'DISCOUNT_1': handle_discount_step_1,
@@ -189,4 +204,4 @@ async def handle_users_reply(event: SimpleBotEvent):
 		'DISCOUNT_6': handle_discount_step_6,
 	}
 	state_handler = states_functions[user_state]
-	os.environ[f'{user_id}_NEXT_STATE'] = await state_handler(event)
+	await storage.put(Key(f'{user_id}_next_state'), value=await state_handler(event))
